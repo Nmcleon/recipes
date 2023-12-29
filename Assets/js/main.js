f3d66104fb1f9c25e20a0d9f0a67010f1cc85ffe
octocatblain <blainmasaku2@gmail.com>
2023-12-29 21:55:57 +0300

    fix: add JS file to assets

---
+++ Assets/js/main.js
@@ -0,0 +1,13 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  var navbar = document.getElementById("navbar");
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+}
